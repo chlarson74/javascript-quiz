b90c78abfccc0e78570492b6b772070b308ec8be
Chris L <chris@larsonwerks.com>
2023-07-06 17:12:01 -0700

    reorganized my js functions andseperated some into smaller functions

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -88,17 +88,36 @@ function selectAnswer(e) {
     }
     questionIndex ++
     if (questionIndex >= questions.length) {
-        document.querySelector(".quizCard").classList.add("hidden")
-        clearInterval(timeInterval)
-        var initials = prompt("enter your initials")
-        var highscore = {
-            initials, time
-        }
-        localStorage.setItem("high score", JSON.stringify(highscore))
-        window.location.reload()
-        return //stops the execution of the statement ends selectAnswer function
+        endGame() 
     }
     setNextQuestion()
+}
+
+function endGame() {
+    document.querySelector(".quizCard").classList.add("hidden")
+    clearInterval(timeInterval)
+    var initials = prompt("enter your initials") //don't use prompt, use an input field
+    var highscore = {
+        initials, time
+    }
+    localStorage.setItem("high score", JSON.stringify(highscore))
+    window.location.reload()
+    return //use for returning a value or ending a function early
+    //stops the execution of the statement ends selectAnswer function
+    //everything disappears
+    //message "all done!" - summary text
+    //input field for initials - eventListener
+    //stop decrementing the time left
+}
+
+function afterSubmit() {
+    //create an edge case check - empty array of previous scores
+    //1 check if existing score 2 if there is an array of previous scores then add newest score to the array 3 overwrite (setItem) and save new aan array an array 3 else creat
+    //create scrore object
+    //initialField.value for the initials value in the score object
+    //insert new score into localStorage ! array of objects  [{initials: "HCW", score: 23}, {initials: "CL": score: 31}] 
+    //show high score 
+    //options to start over and clear high score
 }
 
 function startTimer() {
@@ -114,7 +133,9 @@ function startTimer() {
     //check if the time is zero, if yes - endGame (new function outside of this function)
 }
 
+function showHighScores() {
 
+}
 
 
 //function guess() {
@@ -132,21 +153,9 @@ function newQuestion() {
     //if you are at the end of the array - endGame function
 }
 
-function endGame() {
-    //everything disappears
-    //message "all done!" - summary text
-    //input field for initials - eventListener
-    //stop decrementing the time left
-}
 
-function afterSubmit() {
-    //create an edge case check - empty array of scores
-    //create scrore object
-    //initialField.value for the initials value in the score object
-    //insert new score into localStorage ! array of objects  [{initials: "HCW", score: 23}, {initials: "CL": score: 31}] 
-    //show high score 
-    //options to start over and clear high score
-}
+
+
 
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
